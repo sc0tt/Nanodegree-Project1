0478bc96fe616303b4933023791b2a1ec08ab811
Scott Adie <scottynosabe@gmail.com>
2015-06-13 21:47:42 -0400

    Implemented searching and top 10 songs.

--- app/src/main/java/io/adie/project1/ArtistSearch.java
+++ app/src/main/java/io/adie/project1/ArtistSearch.java
@@ -1,17 +1,118 @@
 package io.adie.project1;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyCallback;
+import kaaes.spotify.webapi.android.SpotifyError;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.client.Response;
 
 
-public class ArtistSearch extends ActionBarActivity {
+public class ArtistSearch extends AppCompatActivity {
+    public final static String ARTIST_ID = "com.mycompany.myfirstapp.ARTIST_ID";
+    public final static String ARTIST_NAME = "com.mycompany.myfirstapp.ARTIST_NAME";
+    public final static String ARTIST_RESULTS = "com.mycompany.myfirstapp.RESULTS";
+    static final String TAG = ArtistSearch.class.getSimpleName();
+    final Runnable failedSearch = new Runnable() {
+        @Override
+        public void run() {
+            Toast.makeText(getApplicationContext(), getString(R.string.no_artists), Toast.LENGTH_SHORT).show();
+        }
+    };
+    SpotifyApi api = new SpotifyApi();
+    SpotifyService spotify = api.getService();
+    List<Artist> artists;
+    ListView artistListView;
+    EditText searchTermView;
+    ArtistListAdapter adapter;
+
+    /*@Override
+    public void onSaveInstanceState(Bundle savedState) {
+
+        super.onSaveInstanceState(savedState);
+
+        // Note: getValues() is a method in your ArrayAdaptor subclass
+        savedState.putSerializable(ARTIST_RESULTS, adapter);
+
+    }*/
+    final Runnable updateResult = new Runnable() {
+        @Override
+        public void run() {
+            adapter.notifyDataSetChanged();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_artist_search);
+        artistListView = (ListView) findViewById(R.id.artist_list);
+        searchTermView = (EditText) findViewById(R.id.artist_search);
+        artists = new ArrayList<Artist>();
+
+        if (savedInstanceState != null) {
+            adapter = (ArtistListAdapter) savedInstanceState.getSerializable(ARTIST_RESULTS);
+        } else {
+            adapter = new ArtistListAdapter(this, artists);
+        }
+
+
+        artistListView.setAdapter(adapter);
+
+        artistListView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                Artist chosenArtist = (Artist) adapter.getItem(position);
+                Intent intent = new Intent(getApplicationContext(), TopTracks.class);
+                intent.putExtra(ARTIST_ID, chosenArtist.id);
+                intent.putExtra(ARTIST_NAME, chosenArtist.name);
+                startActivity(intent);
+            }
+        });
+
+        searchTermView.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    Log.d(TAG, "Searching for " + v.getText());
+                    artists.clear();
+                    adapter.notifyDataSetChanged();
+                    ArtistSearchTask runner = new ArtistSearchTask();
+                    runner.execute(v.getText().toString());
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -34,5 +135,108 @@ public class ArtistSearch extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class ArtistListAdapter extends BaseAdapter implements Serializable {
+
+        Context ctx;
+        List<Artist> artists;
+
+        ArtistListAdapter(Context ctx, List<Artist> artists) {
+            this.ctx = ctx;
+            this.artists = artists;
+        }
+
+        public List<Artist> getArtists() {
+            return artists;
+        }
+
+        @Override
+        public int getCount() {
+            return artists.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return artists.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return artists.indexOf(getItem(pos));
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+
+            ArtistHolder holder = null;
+
+            LayoutInflater mInflater = (LayoutInflater) ctx.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.artist, null);
+                holder = new ArtistHolder();
+                holder.tvArtist = (TextView) convertView.findViewById(R.id.artist_name);
+                holder.ivArtist = (ImageView) convertView.findViewById(R.id.artist_image);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ArtistHolder) convertView.getTag();
+            }
+
+
+            Artist artist = artists.get(pos);
+
+            holder.tvArtist.setText(artist.name);
+            if (artist.images.size() > 0) {
+                Picasso.with(ctx)
+                        .load(artist.images.get(0).url)
+                        .into(holder.ivArtist);
+            }
+
+            return convertView;
+        }
+
+        public class ArtistHolder {
+            ImageView ivArtist;
+            TextView tvArtist;
+        }
+    }
+
+    private class ArtistSearchTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            spotify.searchArtists(params[0], new SpotifyCallback<ArtistsPager>() {
+                @Override
+                public void failure(SpotifyError spotifyError) {
+                    runOnUiThread(failedSearch);
+                }
+
+                @Override
+                public void success(ArtistsPager artistsPager, Response response) {
+                    List<Artist> results = artistsPager.artists.items;
+                    if (results.size() > 0) {
+                        artists.addAll(results);
+                        runOnUiThread(updateResult);
+                    } else {
+                        runOnUiThread(failedSearch);
+                    }
+
+                }
+            });
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
     }
 }
---
+++ app/src/main/java/io/adie/project1/TopTracks.java
@@ -0,0 +1,217 @@
+package io.adie.project1;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.Tracks;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+public class TopTracks extends AppCompatActivity {
+    static final String TAG = ArtistSearch.class.getSimpleName();
+    final Runnable failedSearch = new Runnable() {
+        @Override
+        public void run() {
+            Toast.makeText(getApplicationContext(), getString(R.string.no_songs_found), Toast.LENGTH_SHORT).show();
+        }
+    };
+    String artistName;
+    String artistId;
+    SpotifyApi api = new SpotifyApi();
+    SpotifyService spotify = api.getService();
+    List<Track> tracks;
+    ListView songListView;
+    TrackListAdapter adapter;
+    final Runnable updateResult = new Runnable() {
+        @Override
+        public void run() {
+            adapter.notifyDataSetChanged();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_tracks);
+
+        Intent intent = getIntent();
+        artistName = intent.getStringExtra(ArtistSearch.ARTIST_NAME);
+        artistId = intent.getStringExtra(ArtistSearch.ARTIST_ID);
+
+        actionBarSetup(artistName);
+
+        songListView = (ListView) findViewById(R.id.song_list);
+        tracks = new ArrayList<Track>();
+        adapter = new TrackListAdapter(this, tracks);
+        songListView.setAdapter(adapter);
+
+        TopTenSongTask task = new TopTenSongTask();
+        task.execute(artistId);
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void actionBarSetup(String subtitle) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            getSupportActionBar().setSubtitle(subtitle);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_top_tracks, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class TrackListAdapter extends BaseAdapter {
+
+        Context ctx;
+        List<Track> tracks;
+
+        TrackListAdapter(Context ctx, List<Track> tracks) {
+            this.ctx = ctx;
+            this.tracks = tracks;
+        }
+
+        public List<Track> getTracks() {
+            return tracks;
+        }
+
+        @Override
+        public int getCount() {
+            return tracks.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return tracks.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return tracks.indexOf(getItem(pos));
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+
+            TrackHolder holder = null;
+
+            LayoutInflater mInflater = (LayoutInflater) ctx.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.song, null);
+                holder = new TrackHolder();
+                holder.tvSongName = (TextView) convertView.findViewById(R.id.song_name);
+                holder.tvSongAlbum = (TextView) convertView.findViewById(R.id.song_album);
+                holder.ivAlbumArt = (ImageView) convertView.findViewById(R.id.song_image);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (TrackHolder) convertView.getTag();
+            }
+
+
+            Track track = tracks.get(pos);
+
+            holder.tvSongName.setText(track.name);
+            holder.tvSongAlbum.setText(track.album.name);
+            if (track.album.images.size() > 0) {
+                Picasso.with(ctx)
+                        .load(track.album.images.get(0).url)
+                        .into(holder.ivAlbumArt);
+            }
+
+            return convertView;
+        }
+
+        public class TrackHolder {
+            ImageView ivAlbumArt;
+            TextView tvSongName;
+            TextView tvSongAlbum;
+        }
+    }
+
+    private class TopTenSongTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            Map<String, Object> httpOptions = new HashMap<String, Object>();
+            httpOptions.put("country", "US");
+            spotify.getArtistTopTrack(params[0], httpOptions, new Callback<Tracks>() {
+                @Override
+                public void success(Tracks resultingTracks, Response response) {
+                    List<Track> results = resultingTracks.tracks;
+                    if (results.size() > 0) {
+                        tracks.addAll(results);
+                        runOnUiThread(updateResult);
+                    } else {
+                        runOnUiThread(failedSearch);
+                    }
+                    runOnUiThread(updateResult);
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+                    runOnUiThread(failedSearch);
+                }
+            });
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+}
